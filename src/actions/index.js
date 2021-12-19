e1bbd03b30781d11eddaacf0ed6ac830fb73b443
Alexander King <alex@freepazu.com>
2021-12-19 15:39:17 -0600

    buildout

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+/*
+export const demoOn = () => {
+    return {
+        type: "DEMO_ON"
+    }
+}
+
+
+export const tokenStore = (tokenList) => {
+    return {
+        type: "TOKEN_APPEND",
+        payload: tokenList
+    }
+}
+
+
+
+*/  //Examples
+
